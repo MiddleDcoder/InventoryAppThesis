2416ef080fdc547bf7722f658543d05cf98afcd9
MiddleDcoder <33019593+MiddleDcoder@users.noreply.github.com>
2019-02-25 00:20:35 +0800

    commited

--- app/src/main/java/com/example/jla/inventoryappthesis/AdminDashboard.java
+++ app/src/main/java/com/example/jla/inventoryappthesis/AdminDashboard.java
@@ -3,12 +3,15 @@ package com.example.jla.inventoryappthesis;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class AdminDashboard extends AppCompatActivity {
 
     String UsernameHolder;
     TextView TextViewUsername;
+    Button btn_inboundLogistics;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +26,18 @@ public class AdminDashboard extends AppCompatActivity {
 
         TextViewUsername.setText(TextViewUsername.getText().toString()+UsernameHolder);
 
+        btn_inboundLogistics = findViewById(R.id.btn_inboundLogistics);
+
+        btn_inboundLogistics.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(AdminDashboard.this, InboundLogisticsActivity.class);
+                startActivity(intent);
+            }
+        });
+
 
     }
+
+
 }

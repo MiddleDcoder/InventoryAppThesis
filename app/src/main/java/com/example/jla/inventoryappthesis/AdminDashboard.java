b021e78a7d02bfde4b55bc4e337a5190772d6660
MiddleDcoder <33019593+MiddleDcoder@users.noreply.github.com>
2019-02-27 01:52:38 +0800

    commited new no error

--- app/src/main/java/com/example/jla/inventoryappthesis/AdminDashboard.java
+++ app/src/main/java/com/example/jla/inventoryappthesis/AdminDashboard.java
@@ -1,22 +1,33 @@
 package com.example.jla.inventoryappthesis;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.PopupMenu;
 import android.widget.TextView;
 
 public class AdminDashboard extends AppCompatActivity {
 
     String UsernameHolder;
     TextView TextViewUsername;
+    ImageButton more;
     Button btn_inboundLogistics, btn_outboundLogistics, btn_operations, btn_inventoryStocks, btn_statisticalReports;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_dashboard);
+
+        //full screen,
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         TextViewUsername = findViewById(R.id.textViewUsername);
 
@@ -31,6 +42,8 @@ public class AdminDashboard extends AppCompatActivity {
         btn_operations = findViewById(R.id.btn_operations);
         btn_inventoryStocks = findViewById(R.id.btn_inventory_stock);
         btn_statisticalReports = findViewById(R.id.btn_statistical_reports);
+
+        more = findViewById(R.id.more);
 
         btn_inboundLogistics.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -73,7 +86,60 @@ public class AdminDashboard extends AppCompatActivity {
         });
 
 
+        more.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final PopupMenu popupMenu = new PopupMenu(AdminDashboard.this, more);
+                popupMenu.getMenuInflater().inflate(R.menu.pop_menu, popupMenu.getMenu());
+
+                popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem menuItem) {
+
+                        Intent intent, chooser;
+                        int id = menuItem.getItemId();
+                        if (id == R.id.feedback){
+                            intent = new Intent(Intent.ACTION_SEND);
+                            intent.setData(Uri.parse("mailto:"));
+                            String[] to = {"barentoy55@gmail.com"}; // my email
+                            intent.putExtra(Intent.EXTRA_EMAIL, to);
+                            intent.setType("message/rfc822");
+                            chooser = Intent.createChooser(intent, "Send Feedback");
+                            startActivity(chooser);
+
+                        }
+
+                        if (id == R.id.signOut){
+                            intent = new Intent(AdminDashboard.this, MainActivity.class);
+                            startActivity(intent);
+
+                        }
+
+                        return true;
+                    }
+                });
+                popupMenu.show();
+            }
+
+        });
+
+
     }
 
 
+    public void onClickInboundLogistics(View view) {
+
+    }
+
+    public void onClickOutboundLogistics(View view) {
+    }
+
+    public void onClickOperations(View view) {
+    }
+
+    public void onClickInventoryStocks(View view) {
+    }
+
+    public void onClickStatisticalReport(View view) {
+    }
 }
---
+++ app/src/main/java/com/example/jla/inventoryappthesis/I_B_AddData.java
@@ -0,0 +1,13 @@
+package com.example.jla.inventoryappthesis;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class I_B_AddData extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_i__b__add_data);
+    }
+}
